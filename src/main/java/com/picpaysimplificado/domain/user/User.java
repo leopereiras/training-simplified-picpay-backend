3dd71b2bad34bc64fb9fcda8db19270a897628f7
Leonardo Pereira <leonardopereirasantos@hotmail.com>
2024-01-13 22:29:08 -0300

    Creating user entity

---
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,32 @@
+package com.picpaysimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType usertype;
+
+}
