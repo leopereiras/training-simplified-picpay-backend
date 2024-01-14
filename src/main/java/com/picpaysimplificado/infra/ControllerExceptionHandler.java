a91c6e49f23b4494d5447590cdd73e04de99bc39
Leonardo Pereira <leonardopereirasantos@hotmail.com>
2024-01-14 14:49:50 -0300

    Adding Exception controller

---
+++ src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.picpaysimplificado.infra;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.picpaysimplificado.domain.dtos.ExceptionDTO;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestController
+public class ControllerExceptionHandler {
+
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Já existe um registro com esse valor", "400");
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+}
